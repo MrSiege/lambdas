c9c3914d721669531e3caa80b9fff18c71c8ea94
wei <18371143179@163.com>
2020-01-13 16:28:11 +0800

    修改声明文件

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,7 @@
  * @param {number} index 索引值
  * @return {object} 执行结果
  * */
-declare function nth(dataSource: [], index: number): any;
+export function nth(dataSource: [], index: number): any;
 
 /**
  * 接收一个数据源参数与遍历行为参数，依次对数据源的每一项掉用遍历行为函数
@@ -14,7 +14,7 @@ declare function nth(dataSource: [], index: number): any;
  * @param {function} itemAction 遍历行为
  * @return {void}
  * */
-declare function each(dataSource: [], itemAction: () => void): void;
+export function each(dataSource: [], itemAction: () => void): void;
 
 /**
  * 接收一个数据源参数与遍历行为参数，依次对数据源的每一项掉用遍历行为函数
@@ -22,7 +22,7 @@ declare function each(dataSource: [], itemAction: () => void): void;
  * @param {function} itemAction 遍历行为
  * @return {object} 执行结果
  * */
-declare function map(dataSource: [], itemAction: () => any): any[];
+export function map(dataSource: [], itemAction: () => any): any[];
 
 /**
  * 接收一个元素和一个数组，将元素拼接到数组的前方后返回
@@ -30,7 +30,7 @@ declare function map(dataSource: [], itemAction: () => any): any[];
  * @param {array} arr 数组
  * @return {array} 拼接后返回的元素
  * */
-declare function construct(elem: any, arr: any[]): any[];
+export function construct(elem: any, arr: any[]): any[];
 
 /**
  * 接收一个数据源参数与函数，返回按照排序函数指定数据进行排序的源数据
@@ -38,7 +38,7 @@ declare function construct(elem: any, arr: any[]): any[];
  * @param {function} pickup 数据拾取函数
  * @return {object} 排序结果
  * */
-declare function sortBy(dataSource: any[], pickup: (v: any) => number): any[];
+export function sortBy(dataSource: any[], pickup: (v: any) => number): any[];
 
 
 
@@ -51,7 +51,7 @@ declare function sortBy(dataSource: any[], pickup: (v: any) => number): any[];
  * @param {function} pred 谓词函数
  * @return {boolean} 运算结果
  * */
-declare function allOf(dataSource: any[] | object, pred: (v: any) => boolean): boolean;
+export function allOf(dataSource: any[] | object, pred: (v: any) => boolean): boolean;
 
 /**
  * 接收一个集合参数与一个谓词，当对于集合中有一个元素谓词函数返回 true 时，返回 true，否则返回 false
@@ -59,7 +59,7 @@ declare function allOf(dataSource: any[] | object, pred: (v: any) => boolean): b
  * @param {function} pred 谓词函数
  * @return {boolean} 运算结果
  * */
-declare function anyOf(dataSource:any[] | object, pred: (v: any) => boolean): boolean;
+export function anyOf(dataSource:any[] | object, pred: (v: any) => boolean): boolean;
 
 /**
  * 接收一个数据源参数与谓词函数，返回该谓词为 true 时的所有元素
@@ -67,7 +67,7 @@ declare function anyOf(dataSource:any[] | object, pred: (v: any) => boolean): bo
  * @param {function} pred 谓词函数
  * @return {object} 符合条件的元素
  * */
-declare function filter(dataSource: any[] | object, pred: (v: any) => boolean): any[]; 
+export function filter(dataSource: any[] | object, pred: (v: any) => boolean): any[]; 
 
 /**
  * 接收一个数据源参数与谓词函数，返回该谓词为 true 时的第一个元素
@@ -75,14 +75,14 @@ declare function filter(dataSource: any[] | object, pred: (v: any) => boolean): 
  * @param {function} pred 谓词函数
  * @return {object} 符合条件的第一个元素
  * */
-declare function find(dataSource: any[] | object, pred: (v: any) => boolean): any;
+export function find(dataSource: any[] | object, pred: (v: any) => boolean): any;
 
 /**
  * 接收一个返回谓词的函数参数，返回一个反转谓词参数执行结果的参数(补集)
  * @param {function} pred 返回谓词的函数参数
  * @return {function} Inversion results function
  * */
-declare function complement(pred: (v: any) => boolean): (v: any) => boolean; 
+export function complement(pred: (v: any) => boolean): (v: any) => boolean; 
 
 /**
  * 接收一个数据源参数与函数，返回按照数据拾取函数返回数据进行分组的源数据
@@ -90,7 +90,7 @@ declare function complement(pred: (v: any) => boolean): (v: any) => boolean;
  * @param {function} pickup 数据拾取函数
  * @return {object} 排序结果
  * */
-declare function groupBy(dataSource: any[] | object, pickup: (v : any) => string): object;
+export function groupBy(dataSource: any[] | object, pickup: (v : any) => string): object;
 
 /**
  * 接收一个数据源参数与函数，返回按照的数据拾取函数返回数据进行统计数量的源数据
@@ -98,7 +98,7 @@ declare function groupBy(dataSource: any[] | object, pickup: (v : any) => string
  * @param {function} pickup 数据拾取函数
  * @return {object} 排序结果
  * */
-declare function countBy(dataSource: any[] | object, pickup: (v: any) => string): object;
+export function countBy(dataSource: any[] | object, pickup: (v: any) => string): object;
 
 /**
  * 获取一个对象数组指定键的值
@@ -106,7 +106,7 @@ declare function countBy(dataSource: any[] | object, pickup: (v: any) => string)
  * @param {string} targetKey 目标键
  * @return {array} 值的数组
  * */
-declare function pluck(objects: any[] | object, targetKey: string): any[];
+export function pluck(objects: any[] | object, targetKey: string): any[];
 
 /**
  * 接收一个最佳值的比较规则函数与一个数据源, 返回数据源中的最佳值
@@ -114,7 +114,7 @@ declare function pluck(objects: any[] | object, targetKey: string): any[];
  * @param {function} fun 比较谓词
  * @return {object} 数据源中的最佳值
  * */
-declare function best(dataSource: any[] | object, fun: (v: any) => boolean): any;
+export function best(dataSource: any[] | object, fun: (v: any) => boolean): any;
 
 /**
  * 将一个数据源迭代归结为一个单一的值
@@ -124,7 +124,7 @@ declare function best(dataSource: any[] | object, fun: (v: any) => boolean): any
  * @param {object} context 上下文绑定
  * @return {object} 结果
  * */
-declare function reduce(
+export function reduce(
   dataSource: any[] | object, 
   iterator: (
     iterativeValue: any, 
@@ -145,7 +145,7 @@ declare function reduce(
  * @param {function} pred 返回谓词的函数参数
  * @return {function} 比较函数
  * */
-declare function comparator(pred: (x: any, y: any) => any): (x: any, y: any) => number;
+export function comparator(pred: (x: any, y: any) => any): (x: any, y: any) => number;
 
 /**
  * 返回一个常量的函数
@@ -153,7 +153,7 @@ declare function comparator(pred: (x: any, y: any) => any): (x: any, y: any) => 
  * @param {object} value 定义需要返回的常量
  * @return {object} 常量的值
  * */
-declare function always(value: any): () => any;
+export function always(value: any): () => any;
 
 /**
  * 接收一个前馈函数与值的生成函数，当生成的值符合前馈函数的条件时，停止执行当前函数
@@ -161,14 +161,14 @@ declare function always(value: any): () => any;
  * @param {function} init 值的初始化函数
  * @return {array} result 结果
  * */
-declare function iterateUntil(feedForward: (value: any, index: number) => boolean, init: () => any): any[];
+export function iterateUntil(feedForward: (value: any, index: number) => boolean, init: () => any): any[];
 
 /**
  * 接收一个函数及一些额外的参数，并返回一个只是调用给定的原始函数的函数
  * @param {function} fun 原始函数
  * @return {function}　经过包装的原始函数
  * */
-declare function fnull(fun: (...args: any) => any, ...args: any): (...args: any) => any;
+export function fnull(fun: (...args: any) => any, ...args: any): (...args: any) => any;
 
 /**
  * 创建一个验证器
@@ -178,10 +178,10 @@ declare function fnull(fun: (...args: any) => any, ...args: any): (...args: any)
  * */
 
 interface validator{
-  (message: string, fun: (...args) => boolean): (...args) => boolean;
+  (message: string, fun: (...args: any) => boolean): (...args: any) => boolean;
 }
 
-declare function validator(message: string, fun: (...args) => boolean): (...args) => boolean;
+export function validator(message: string, fun: (...args: any) => boolean): (...args: any) => boolean;
 
 /**
  * 接收一组谓词函数，返回一个验证函数。返回的验证函数在给定对象上执行每个谓词，
@@ -189,7 +189,7 @@ declare function validator(message: string, fun: (...args) => boolean): (...args
  * 如果所有的谓词返回 true, 那么最终返回的结果是一个空数组。否则，结果为错误消息的数组。
  * @return {function} 聚合了谓词函数的验证函数
  * */
-declare function checker(...validators: validator[]): (targetValidationData: any) => any;
+export function checker(...validators: validator[]): (targetValidationData: any) => any;
 
 
 
@@ -200,63 +200,63 @@ declare function checker(...validators: validator[]): (targetValidationData: any
  * @param {object} dataSource 源数据
  * @return {array} 转换后的结果
  * */
-declare function toArray(dataSource: object | Map<any, any> | any[]): any[];
+export function toArray(dataSource: object | Map<any, any> | any[]): any[];
 
 /**
  * 判断传递进来的参数是否是字符串数据类型
  * @param {object} string 参数
  * @return {boolean} 参数是否为字符串
  * */
-declare function isString(string: any): boolean;
+export function isString(string: any): boolean;
 
 /**
  * 判断传递进来的参数是否是数字数据类型
  * @param {object} number 参数
  * @return {boolean} 参数是否为数字
  * */
-declare function isNumber(number: any): boolean;
+export function isNumber(number: any): boolean;
 
 /**
  * 判断传递进来的参数是否是 NaN 类型
  * @param {number} number 参数
  * @return {boolean} 参数是否为 NaN 类型
  * */
-declare function isNaN(number: any): boolean;
+export function isNaN(number: any): boolean;
 
 /**
  * 判断传递进来的参数是否是 Array 类型
  * @param {object} array 参数
  * @return {boolean} 参数是否为 Array 类型
  * */
-declare function isArray(array: any): boolean;
+export function isArray(array: any): boolean;
 
 /**
  * 判断传递进来的参数是否为 function 类型
  * @param {function} func 函数参数
  * @return {boolean} 参数是否为函数类型的数据
  * */
-declare function isFunction(func: any): boolean;
+export function isFunction(func: any): boolean;
 
 /**
  * 判断传递进来的参数是否是支持数字索引的
  * @param {object} data 参数
  * @return {boolean} 参数是否为 Array 类型
  * */
-declare function isIndexed(data: any): boolean;
+export function isIndexed(data: any): boolean;
 
 /**
  * 判断传递进来的参数是否存在（存在有效值）
  * @param {object} x 参数
  * @return {boolean} 参数是否存在有效值
  * */
-declare function exist(x: any): boolean;
+export function exist(x: any): boolean;
 
 /**
  * 判断传递进来的参数是否为 true 的同义词
  * @param {object} x 参数
  * @return {boolean} 参数是否为 true 的同义词
  * */
-declare function truest(x: any): boolean;
+export function truest(x: any): boolean;
 
 /**
  * 根据第一个谓词参数执行第二个函数参数并返回结果
@@ -264,14 +264,14 @@ declare function truest(x: any): boolean;
  * @param {function} action 函数参数
  * @return {object} 执行结果
  * */
-declare function doWhen(condition: any, action: () => any): any;
+export function doWhen(condition: any, action: () => any): any;
 
 /**
  * 深度复制一个源对象到目标对象，在使用递归算法时应尽量避免深层递归，递归过深会耗尽系统栈的内存，造成栈溢出。
  * @param {object} sourceObject 源对象
  * @return {object} 新对象
  * */
-declare function duplicate(sourceObject: any) : any;
+export function duplicate(sourceObject: any) : any;
 
 
 
@@ -282,14 +282,14 @@ declare function duplicate(sourceObject: any) : any;
  * @param {object} object 目标对象
  * @return {array} 对象所有键的数组
  * */
-declare function keys(object: object): string[];
+export function keys(object: object): string[];
 
 /**
  * 获取一个对象的所有值
  * @param {object} object 目标对象
  * @return {array} 对象所有值的数组
  * */
-declare function values(object: object): any[]; 
+export function values(object: object): any[]; 
 
 
 
@@ -299,36 +299,36 @@ declare function values(object: object): any[];
  * 往 html 文档中写一行内容
  * @return {void}
  * */
-declare function documentWriteln(...args: any): void;
+export function documentWriteln(...args: any): void;
 
 /**
  * 往 html 文档中写入内容
  * @return {void}
  * */
-declare function documentWrite(...args: string[]): void;
+export function documentWrite(...args: string[]): void;
 
 /**
  * 抛出一个异常
  * @param {string} thing 异常描述
  * @return {void}
  * */
-declare function fail(thing: string): never;
+export function fail(thing: string): never;
 
 /**
  * 向控制台输出一段警告文字
  * @param {string} thing 警告描述
  * @return {void}
  * */
-declare function warning(thing: string): void;
+export function warning(thing: string): void;
 
 /**
  * 向控制台输出一段提示文字
  * @param {string} thing 提示文字描述
  * @return {void}
  * */
-declare function note(...args: string[]): void;
+export function note(...args: string[]): void;
 
-declare namespace G{
+export namespace G{
   // Array namespace
 
   /**
@@ -509,10 +509,10 @@ declare namespace G{
    * */
 
   interface validator{
-    (message: string, fun: (...args) => boolean): (...args) => boolean;
+    (message: string, fun: (...args: any) => boolean): (...args: any) => boolean;
   }
 
-  function validator(message: string, fun: (...args) => boolean): (...args) => boolean;
+  function validator(message: string, fun: (...args: any) => boolean): (...args: any) => boolean;
 
   /**
    * 接收一组谓词函数，返回一个验证函数。返回的验证函数在给定对象上执行每个谓词，
