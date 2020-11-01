1c034838ca1e718c4bd77816ec7120e1e905d2dd
william <18371143179@163.com>
2020-11-01 19:45:57 +0800

    umd

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,25 @@
 const path = require("path");
-const webpack = require("webpack");
+const package = require("./package");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
   stats: 'normal',
-  entry: { lambdas: "./source/index.js" },
+  mode: 'production',
+  entry: './source/index.js',
   output: {
-    publicPath: "",
-    filename: "[name].[hash].min.js",
-    path: path.resolve(__dirname, "./dist/")
+    filename: 'lambdas.min.js',
+    path: path.resolve(__dirname, `./dist/${package.version}`)
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        enforce: 'pre',
-        use: ['source-map-loader'],
-      },
-      {
-        test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: 'babel-loader',
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.SplitChunksPlugin({name: "commons", filename: "commons.js"}),
-    new webpack.optimize.MinChunkSizePlugin({minChunkSize: 1000}),
+  plugins: [ 
     new CleanWebpackPlugin(),
   ]
 };
